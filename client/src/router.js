a95f69d47c19e91fcd8f9b89b23748365ef3a07c
joaofilipe <joao.filipe@dev.boataround.com>
2022-05-06 22:14:54 +0200

    ADDED: page titles to routes

--- client/src/router.js
+++ client/src/router.js
@@ -4,18 +4,29 @@ const routes =  [
     {
         path: "/",
         name: "All questions",
-        component: () => import("./components/QuestionsList")
+        component: () => import("./components/QuestionsList"),
+        meta: {
+            title: 'All questions',
+        }
     },
     {
         path: "/questions",
         name: "Add question",
-        component: () => import("./components/AddQuestion")
+        component: () => import("./components/AddQuestion"),
+        meta: {
+            title: 'Add questions',
+        }
     },
 ];
 
 const router = createRouter({
     history: createWebHistory(),
     routes,
+});
+
+router.beforeEach((to, from, next) => {
+    document.title = to.meta.title;
+    next();
 });
 
 export default router;
